419d110a1aca34cf2ce4024abc6d197465d50aef
emirevec <vecchioemiliano@gmail.com>
2025-05-15 16:03:16 -0300

    feat: add calculateAverageRate.js

--- gsheet-exchange-rates/Code.js
+++ gsheet-exchange-rates/Code.js
@@ -4,8 +4,11 @@ function main(){
 
   try {
     const exchangeRate = fetchBCRAExchangeRateByDate({date: config.today});
+    const averageRate = calculateAverageRate({sheetName: config.reportSheetName, thirtythValue: exchangeRate.value});
+    Logger.log(`${functionName}: Average exchange rate: ${averageRate}`);
     updateExchangeRateInSheet({sheetName: config.reportSheetName, data: exchangeRate});
   } catch (error) {
     Logger.log(`${functionName}: An error occurred: ${error}`);
   }
 }
+
